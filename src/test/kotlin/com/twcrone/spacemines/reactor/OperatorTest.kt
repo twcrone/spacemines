fcc170446a568e3cef7807bf34ce8ea1519f3cda
Todd Crone <todd.crone@kroger.com>
2018-12-24 16:11:29 -0500

    Add concat, merge and zip examples

--- src/test/kotlin/com/twcrone/spacemines/reactor/OperatorTest.kt
+++ src/test/kotlin/com/twcrone/spacemines/reactor/OperatorTest.kt
@@ -3,6 +3,7 @@ package com.twcrone.spacemines.reactor
 import org.junit.Test
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.Duration
 
 class OperatorTest {
 
@@ -25,5 +26,26 @@ class OperatorTest {
     Mono.just(3)
         .flatMapMany { i -> Flux.range(1, i) }
         .subscribe(System.out::println)
+  }
+
+  @Test
+  fun `concat and merge`() {
+    val oneToFive = Flux.range(1, 5).delayElements(Duration.ofMillis(200))
+    val sixToTen = Flux.range(6, 5).delayElements(Duration.ofMillis(400))
+
+    Flux.merge(oneToFive, sixToTen)
+        .subscribe(System.out::println)
+
+    //Thread.sleep(4000)
+  }
+
+  @Test
+  fun `zip`() {
+    val oneToFive = Flux.range(1, 5)
+    val sixToTen = Flux.range(6, 5)
+
+    oneToFive.zipWith(sixToTen)
+        .log()
+        .subscribe()
   }
 }
