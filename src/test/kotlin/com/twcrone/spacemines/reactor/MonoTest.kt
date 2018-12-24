e5914aaeec9efaf363ecdd2f0d9f8722170fb594
Todd Crone <todd.crone@kroger.com>
2018-12-24 11:43:39 -0500

    Mono examples

--- src/test/kotlin/com/twcrone/spacemines/reactor/MonoTest.kt
+++ src/test/kotlin/com/twcrone/spacemines/reactor/MonoTest.kt
@@ -1,11 +1,28 @@
 package com.twcrone.spacemines.reactor
 
 import org.junit.Test
+import reactor.core.publisher.Mono
+import java.lang.RuntimeException
 
 class MonoTest {
 
   @Test
-  fun firstMono() {
-    //Mono.just()
+  fun monoWithConsumer() {
+    Mono.just("A")
+        .log()
+        .subscribe(System.out::println)
   }
+
+  @Test
+  fun `checked exception mono`() {
+    Mono.error<Exception>(Exception("BLAH"))
+        .doOnError{ e -> println("Error: $e") }
+//        .onErrorResume { e ->
+//          println("Handling Error: $e")
+//          return Mono.just("Something")
+//        }
+        .log()
+        .subscribe(System.out::println)
+  }
+
 }
